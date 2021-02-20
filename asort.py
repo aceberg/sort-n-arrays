f986f7d7fa7cd90998b9bbe14dd8c655825e4847
aceberg <1502200+aceberg@users.noreply.github.com>
2021-02-20 16:05:14 +0700

    check n is digit

--- asort.py
+++ asort.py
@@ -3,36 +3,47 @@
 import sys
 from random import randint
 
-max_size = 10 # MUST BE BIGGER THEN N
-arr_size = []
+max_size = 100 # MUST BE BIGGER THEN N
+max_value = 100
 
-def gen_sizes(n):
+def gen_sizes(n):	# generate a list of unique sizes
+	arr_size = []
 	for i in range(0,n):
 		k = randint(2, max_size)
 		while k in arr_size:
 			k = randint(2, max_size)
 		arr_size.append(k)
+	return arr_size
 
 
-def gen_array(k):
+def gen_array(k):	# generate one array
 	one_array = []
-	for i in range(0,arr_size[k]):
-		one_array.append(randint(0, 100))
+	for i in range(0,k):
+		one_array.append(randint(-max_value, max_value))
 	return one_array
 
 def main():
-	one_array = []
 	n_arrays = []
-	n = int(sys.argv[1])
+	arr_size = []	
+	k = ''
+	
+	if len(sys.argv) == 2:
+		k = sys.argv[1]
+	while not k.isdigit():
+		print('Please input a number:')
+		k = input()
+	n = int(k)
 	print('n =',n)
-	gen_sizes(n)
+	
+	arr_size = gen_sizes(n)
 	print('array sizes:',arr_size)
 	
+	print('\nGenerated arrays')	
 	for i in range(0,n):
-		one_array=gen_array(i)
-		print(i,':',one_array)
-		n_arrays.append(one_array)
+		n_arrays.append(gen_array(arr_size[i]))
+		print(i,':',n_arrays[i])
 
+	print('\nSorted arrays')
 	for i in range(0,n):
 		if (i % 2 == 0):
 			n_arrays[i].sort()
